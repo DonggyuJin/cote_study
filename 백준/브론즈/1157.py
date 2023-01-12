be7f4a4760aa7782ec47679058089c74fba09089
jdg70 <jdk2531@gmail.com>
2023-01-12 23:04:20 +0900

    add 1157

--- 백준/브론즈/1157.py
+++ 백준/브론즈/1157.py
@@ -1,5 +1,9 @@
-# data=list(input())
-# num=[]
-# for i in range(len(data)):
-#     num.append(data.count(data[i]))
-# print(num)
+data=input().lower()
+new=list(set(data))
+cnt=[]
+for i in new:
+    count=data.count(i)
+    cnt.append(count)
+print(new, cnt)
+if cnt.count(max(cnt))>=2: print('?')
+else: print(new[(cnt.index(max(cnt)))].upper())
