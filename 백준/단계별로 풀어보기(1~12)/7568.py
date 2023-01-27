74af8d5b4ee1beed566b1f211ab61b77edd0fa49
jdg70 <jdk2531@gmail.com>
2023-01-27 16:12:05 +0900

    백준 추가

---
+++ 백준/단계별로 풀어보기(1~12)/7568.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+N=int(input())
+data=[list(map(int, input().split())) for _ in range(N)]
+result=[]
+
+for i in range(N):
+    count=0
+    for j in range(N):
+        if(data[i][0]<data[j][0] and data[i][1]<data[j][1]):
+            count+=1
+    print(result)
+    result.append(count+1)
+
+for i in result: print(i, end=' ')
