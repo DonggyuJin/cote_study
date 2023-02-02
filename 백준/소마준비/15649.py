782dabd523e071be521ee9a1fadae98b11caf745
jdg70 <jdk2531@gmail.com>
2023-02-02 13:25:05 +0900

    백준 추가

--- 백준/소마준비/15649.py
+++ 백준/소마준비/15649.py
@@ -1,6 +1,6 @@
-from itertools import permutations
+from itertools import permutations, combinations
 
 n,m=map(int, input().split())
-data = list(permutations([i for i in range(1, n+1)], m))
+data = list(combinations([i for i in range(1, n+1)], m))
 
 for i in range(len(data)): print(*data[i])
