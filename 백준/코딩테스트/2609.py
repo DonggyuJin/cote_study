3156d7e6a707b7da1595e7fc80dc675c28f5d746
jdg70 <jdk2531@gmail.com>
2023-02-12 22:11:39 +0900

    백준 추가

---
+++ 백준/코딩테스트/2609.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def gcd(x, y):
+  while(y): x, y = y, x%y
+  return x
+
+def lcm(x, y):
+  return (x*y) // gcd(x, y)
+
+x, y = map(int, input().split())
+
+print(gcd(x, y))
+print(lcm(x, y))
