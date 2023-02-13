6b1a5481f28e3b0d08cacc1641bfe98767714e68
jdg70 <jdk2531@gmail.com>
2023-02-13 21:38:00 +0900

    수정

--- 백준/dfs&bfs/1926.py
+++ 백준/dfs&bfs/1926.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -10,9 +11,10 @@ dx = [1, 0, -1, 0]
 
 def bfs(y, x):
   rs = 1
-  q = [(y, x)]
+  q = deque()
+  q.append((y,x))
   while q:
-    ey, ex = q.pop()
+    ey, ex = q.popleft()
     for k in range(4):
       ny = ey + dy[k]
       nx = ex + dx[k]
