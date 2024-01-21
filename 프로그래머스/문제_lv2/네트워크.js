b6993fad50fc11f45637807b8c0c0610d9cab43f
DonggyuJin <jdk2531@gmail.com>
2024-01-21 13:16:59 +0900

    프로그래머스

---
+++ 프로그래머스/문제_lv2/네트워크.js
@@ -0,0 +1,24 @@
+function solution(n, computers) {
+  var answer = 0;
+
+  const visited = new Array(n).fill(0);
+
+  function dfs(map, visited, node) {
+    visited[node] = 1;
+
+    for (let i = 0; i < map[node].length; i++) {
+      if (map[node][i] && visited[i] === 0) {
+        dfs(map, visited, i);
+      }
+    }
+  }
+
+  for (let i = 0; i < visited.length; i++) {
+    if (visited[i] === 1) continue;
+
+    answer += 1;
+    dfs(computers, visited, i);
+  }
+
+  return answer;
+}
