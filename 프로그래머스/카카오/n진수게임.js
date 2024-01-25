d13b11e3d07c9fc206fba84b7d5fdd3a70242ebd
DonggyuJin <jdk2531@gmail.com>
2024-01-26 08:32:25 +0900

    프로그래머스

---
+++ 프로그래머스/카카오/n진수게임.js
@@ -0,0 +1,16 @@
+function solution(n, t, m, p) {
+  var answer = "";
+
+  let temp = "";
+  for (let i = 0; i < t * t; i++) {
+    temp += i.toString(n);
+  }
+
+  let idx = p - 1;
+  while (answer.length !== t) {
+    answer += temp[idx].toUpperCase();
+    idx += m;
+  }
+
+  return answer;
+}
