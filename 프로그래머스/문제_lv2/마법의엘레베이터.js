e9447702cdc88ac98b3abae0104dae7f85f2e22f
DonggyuJin <jdk2531@gmail.com>
2024-02-09 09:32:38 +0900

    프로그래머스

---
+++ 프로그래머스/문제_lv2/마법의엘레베이터.js
@@ -0,0 +1,26 @@
+function solution(storey) {
+  var answer = 0;
+
+  const list = [];
+  while (storey !== 0) {
+    list.push(storey % 10);
+    storey = parseInt(storey / 10);
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] >= 10) {
+      if (i + 1 < list.length) list[i + 1]++;
+      else answer++;
+    } else if (list[i] < 5) answer += list[i];
+    else if (list[i] > 5) {
+      answer += 10 - list[i];
+      if (i + 1 < list.length) list[i + 1]++;
+      else answer++;
+    } else if (list[i] === 5) {
+      answer += list[i];
+      if (i + 1 < list.length && list[i + 1] >= 5) list[i + 1]++;
+    } else return -1;
+  }
+
+  return answer;
+}
